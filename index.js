071ad919042819e49b706c0f9daf75373f147c36
ChengCK18 <43441027+ChengCK18@users.noreply.github.com>
2021-08-01 16:41:05 +0800

    Testing Heroku Deployment

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ const generateId = () =>{
 
 
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server is running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
